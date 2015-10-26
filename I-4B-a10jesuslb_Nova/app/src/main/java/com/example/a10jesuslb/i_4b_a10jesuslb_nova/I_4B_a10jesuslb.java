39dd9f3480bd81b0ac953a4517b0a95f4be3e460
jmlb23 <contadoinstituto@gmail.com>
2015-10-26 10:24:22 +0100

    empezoa de novo

---
+++ I-4B-a10jesuslb_Nova/app/src/main/java/com/example/a10jesuslb/i_4b_a10jesuslb_nova/I_4B_a10jesuslb.java
@@ -0,0 +1,13 @@
+package com.example.a10jesuslb.i_4b_a10jesuslb_nova;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class I_4B_a10jesuslb extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_i_4_b_a10jesuslb);
+    }
+}
---
+++ I-4B-a10jesuslb_Nova/app/src/test/java/com/example/a10jesuslb/i_4b_a10jesuslb_nova/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.a10jesuslb.i_4b_a10jesuslb_nova;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
